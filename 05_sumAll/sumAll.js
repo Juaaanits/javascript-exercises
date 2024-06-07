20d5e1ac419193804aac404c5403ba66ddc4ed02
Juanito M. Ramos II <juanitoramos113@gmail.com>
2024-06-07 20:06:28 +0800

    solved #2, 3, 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(minimum, maximum) {
+    if (minimum < 0 || maximum < 0) return "ERROR";
+    if (!Number.isInteger(minimum) || !Number.isInteger(maximum)) return "ERROR";
+    if (typeof minimum !== 'number' || typeof maximum !== 'number') return "ERROR";
 
+    let finalSum = 0;
+
+    if (minimum > maximum) {
+        // Swap the values
+        [minimum, maximum] = [maximum, minimum];
+    }
+
+    for (let i = minimum; i <= maximum; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
